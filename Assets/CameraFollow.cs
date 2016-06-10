b2b266cea13851c195fd6753981b4ca6760e2269
Robierto <robert_sebree@yahoo.com>
2016-06-10 18:53:52 -0400

    New Project

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+	// Use this for initialization
+	void Lateupdate()
+    {
+        transform.position = new Vector3(Target.position.x, transform.position.y, Target.position.z);
+    }
+}
